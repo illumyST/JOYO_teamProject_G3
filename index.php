76b31a0812314efba14ff11018bb211fe3693ee7
albert840823 <allen840419@gmail.com>
2023-05-03 11:57:46 +0800

    change

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 echo "test";
-echo "<h3>how are you</h3>";
-echo "<h3>Hello World</h3>";
-echo "<h3>how ae u</h3>";
-echo '<h3>how are you</h3>';
+echo "<h3>haaw are you</h3>";
+echo "<h3>Hello dorld</h3>";
+echo "<h3>how ae yu</h3>";
+echo '<h3>how are y</h3>';
 echo "<h3>how  u</h3>";
 
 ?>
