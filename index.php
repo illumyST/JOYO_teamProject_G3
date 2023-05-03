ac9a4242444b7a9614e8a9fdba2c600a9d1107cc
jackyeh4488 <jackyeh4488@gmail.com>
2023-05-03 13:17:50 +0800

    把你們都刪掉

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
 echo "test";
 echo "<h3>haaw are you</h3>";
-echo "<h3>Hello dorld</h3>";
-echo "<h3>how ae yu</h3>";
-echo '<h3>how are y</h3>';
-echo "<h3>how  u</h3>";
-echo "<h3>我是誰</h3>";
-echo "<h3>我要來偷改</h3>";
-echo "<h3>h u</h3>";
-echo "我來了";
-echo "<h3>how ae yu</h3>";
 ?>
 <h3>how are you</h3>
