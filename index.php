f889bd8a1703c60e58300610055ea436e94ed8df
Chouwill <will92068@gmail.com>
2023-05-03 15:35:37 +0800

    delete

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
-echo "test";
-echo "<h3>haaw are you</h3>";
-echo "<h3>Hello dorld</h3>";
-echo "<h3>how ae yu</h3>";
-echo '<h3>how are y</h3>';
-echo "<h3>how  u</h3>";
-echo "<h3>我是誰</h3>";
-echo "<h3>我要來偷改</h3>";
-echo "<h3>h u</h3>";
-echo "我來了";
-echo "<h3>how ae yu</h3>";
-echo "解衝突";
-echo "新增文字"
 
 ?>
 <h3>how are you</h3>
