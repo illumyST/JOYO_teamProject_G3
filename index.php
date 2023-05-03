8e96b89e12637d2327548d6b0425b3531eb824e0
albert840823 <allen840419@gmail.com>
2023-05-03 11:52:38 +0800

    test

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 echo "test";
-<<<<<<< HEAD
-echo "<h3>how are you</h3>"
-echo "<h3>Hello World</h3>"
-=======
->>>>>>> e20a2bcb0f400988fdcdae1e95635a2a59b432a7
+echo "<h3>how are you</h3>";
+echo "<h3>Hello World</h3>";
 ?>
