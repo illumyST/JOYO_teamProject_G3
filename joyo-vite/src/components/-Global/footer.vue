c7445dd82620979c0a6fc5432055e4458950799c
illumyST <illumy.design@gmail.com>
2023-05-17 12:56:30 +0800

    footer router-link 設置完成 topBtn 回頂設置完成

--- joyo-vite/src/components/-Global/footer.vue
+++ joyo-vite/src/components/-Global/footer.vue
@@ -2,38 +2,89 @@
     <footer>
         <nav class="footer_nav">
             <div class="footer_nav_left">
-                <a href="#"><img class="footer_nav_left_logo" src="@/assets/img/logo.svg" alt=""></a>
+                <RouterLink to="/" >
+                    <img class="footer_nav_left_logo" src="@/assets/img/logo.svg" alt="">
+                </RouterLink>
+
+                <!-- 左側社群 icon -->
                 <ul class="footer_nav_left_icons">
                     <li><a href="#"><i class="fa-brands fa-square-instagram"></i></a></li>
                     <li><a href="#"><i class="fa-brands fa-square-facebook"></i></a></li>
                     <li><a href="#"><i class="fa-brands fa-square-twitter"></i></a></li>
                 </ul>
+
             </div>
 
             <ul class="footer_nav_right">
                 <li>
-                    <a href="#">桌遊商城</a>
+                    <RouterLink to="/product">桌遊商城</RouterLink>
                     <ol>
-                        <li><a href="#">派對遊戲</a></li>
-                        <li><a href="#">輕度策略</a></li>
-                        <li><a href="#">戰爭遊戲</a></li>
-                        <li><a href="#">棋奕遊戲</a></li>
-                        <li><a href="#">主題遊戲</a></li>
-                        <li><a href="#">家庭遊戲</a></li>
-                        <li><a href="#">團隊合作</a></li>
+                        <li>
+                            <RouterLink :to="{ name: 'productCategory', params: { categoryId: 1 } }">
+                                派對遊戲
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink :to="{ name: 'productCategory', params: { categoryId: 2 } }">
+                                輕度策略
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink :to="{ name: 'productCategory', params: { categoryId: 3 } }">
+                                戰爭遊戲
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink :to="{ name: 'productCategory', params: { categoryId: 4 } }">
+                                棋奕遊戲
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink :to="{ name: 'productCategory', params: { categoryId: 5 } }">
+                                主題遊戲
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink :to="{ name: 'productCategory', params: { categoryId: 6 } }">
+                                家庭遊戲
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink :to="{ name: 'productCategory', params: { categoryId: 7 } }">
+                                團隊合作
+                            </RouterLink>
+                        </li>
                     </ol>
                 </li>
                 <li>
-                    <a href="#">討論區</a>
+                    <RouterLink to="/forum">討論區</RouterLink>
                     <ol>
-                        <li><a href="#">心得分享</a></li>
-                        <li><a href="#">揪團區</a></li>
-                        <li><a href="#">發問區</a></li>
-                        <li><a href="#">教學區</a></li>
+                        <li>
+                            <RouterLink :to="{ name: 'forumCategory', params: { categoryId: 1 } }">
+                                心得分享
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink :to="{ name: 'forumCategory', params: { categoryId: 2 } }">
+                                揪團區
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink :to="{ name: 'forumCategory', params: { categoryId: 3 } }">
+                                發問區
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink :to="{ name: 'forumCategory', params: { categoryId: 4 } }">
+                                教學區
+                            </RouterLink>
+                        </li>
                     </ol>
                 </li>
                 <li>
-                    <a href="#">聯絡我們</a>
+                    <RouterLink to="/contact">
+                        聯絡我們
+                    </RouterLink>
                     <span><i class="fa-solid fa-envelope"></i>joyo2023@gmail.com
                     </span><br>
                     <span><i class="fa-solid fa-phone"></i>09123456789</span>
@@ -48,7 +99,6 @@
 </script>
 
 <style lang="scss" scoped>
-
 footer {
     background-color: $orange;
     height: 250px;
@@ -91,6 +141,12 @@ footer {
     color: #fff;
     margin: 2px 8px;
     transition: .2s;
+}
+
+// 讓社群 icon 出現
+.footer_nav_left_icons a i {
+    font-family: "fontawesome";
+    font-weight: 400;
 }
 
 .footer_nav_right {
@@ -161,7 +217,7 @@ footer {
         font-size: 28px;
     }
 
-    .footer_nav_left_icons a i{
+    .footer_nav_left_icons a i {
         font-family: "Font Awesome 6 Free";
         font-weight: 400;
     }
