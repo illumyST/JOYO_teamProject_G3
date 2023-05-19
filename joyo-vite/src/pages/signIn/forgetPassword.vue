c50742c64d05ba0c969fb90dcd2d58e31c84a2cf
illumyST <illumy.design@gmail.com>
2023-05-19 15:38:25 +0800

    註解起來的區域路徑或語法有錯誤，造成無法打包 岳、馨

--- joyo-vite/src/pages/signIn/forgetPassword.vue
+++ joyo-vite/src/pages/signIn/forgetPassword.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="forgetPassword_box">
       <div class="forgetPassword_logo">
-      <img class="forgetPassword_logo_img" src="./assets/img/logo_orange.svg" alt="">
+      <img class="forgetPassword_logo_img" src="@/assets/img/logo_orange.svg" alt="">
       </div>
     <form class="forgetPassword_main" method="post" action="">
       <p class="forgetPassword_p" >請輸入你的電子信箱來取得密碼重置信</p>
