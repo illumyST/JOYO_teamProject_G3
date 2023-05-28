d9aa5211a88a342cf6f830af761b6aad88abce29
Peiyakao <cubearcu@gmail.com>
2023-05-28 22:01:07 +0800

    新增購物車會員登入加入購物車邏輯、首頁熱門商品呈現

--- joyo-vite/PDO/product/Insert.php
+++ joyo-vite/PDO/product/Insert.php
@@ -3,21 +3,67 @@
 include("../connect/conn.php");
 
        //---------------------------------------------------
-       //接收前端來的POST資料
-       $cartItem=($_POST["cartItem"]);
-       $MEMBER_ID=$cartItem->MEMBER_ID;
-       //先取回資料庫購物車資料
-       //建立SQL
-       // $sql = "INSERT INTO member(Account, PWD, CreateDate) VALUES ('王小明', 'abc123', NOW())";
-
-       // //執行
-       // $affectedRow = $pdo->exec($sql); //->與js的"."概念相同，表示下一層
-       // if($affectedRow > 0){
-       //        echo "新增成功!";
-       // }else{
-       //        echo "新增失敗!";
-       // }
-       echo $cartItem;
-       echo $member_ID;
+       //接收前端來的POST的JSON資料
+       $cartItem = file_get_contents('php://input');
+       //將資料格式轉換
+       $cartItemData = json_decode($cartItem, true);
+       //取得加入購物車的商品ID
+       $PRODUCT_ID = $cartItemData['PRODUCT_ID'];
+       //取得會員ID 
+       $MEMBER_ID = $cartItemData['MEMBER_ID'];
+       //測試資料有成功取得
+       echo $PRODUCT_ID ;
+       echo $MEMBER_ID ;
+       //建立一個新的變數存放資料庫數據
+       $tg = new PDO($dsn, $db_user, $db_pass);
+       //查看會員購物車資料庫是否已存在相同商品
+       //SQL指令，搜尋會員編號=$MEMBER_ID的購物車資料
+       $sqlSelecMember = "SELECT * FROM CART where MEMBER_ID=$MEMBER_ID";
+       $cartList=$tg->query($sqlSelecMember);
+       $cartListdata = $cartList->fetchAll();
+       //測試用，看是否成功取得陣列資料
+       //echo print_r($cartListdata, true);
+       //判斷該會員是否購物車已經有商品，如果有要確認商品是否重複
+       if(count($cartListdata)>0){
+              //確認是否有同樣桌遊存在資料庫，如果沒有則新增
+              $sqlSelecMemberProduct = "SELECT * FROM CART where MEMBER_ID=$MEMBER_ID and PRODUCT_ID=$PRODUCT_ID" ;
+              $cartListProduct=$tg->query($sqlSelecMemberProduct);
+              $cartListProductdata = $cartListProduct->fetchAll();
+              //資料庫有相同商品，變更商品數量+1
+              if(count($cartListProductdata)>0){
+                     echo $cartListProductdata[0][0];
+                     //取得該筆購物車的ID
+                     $cartId=$cartListProductdata[0][0];
+                     //取得該筆購物車現行數量
+                     $amount=$cartListProductdata[0][2];
+                     $sqlupdate=" UPDATE `CART` SET `AMOUNT` = $amount+1 WHERE (`CART_ID` = $cartId); ";
+                     $updateRow = $tg->exec($sqlupdate);
+                     if($updateRow > 0){
+                        echo "新增成功!";
+                     }else{
+                        echo "新增失敗!";
+                     }
+              //該會員購物車資料庫沒有有相同商品，新增一筆購物車商品         
+              }else{
+                     $sqlinsert="INSERT INTO `CART` (`PRODUCT_ID`, `AMOUNT`, `MEMBER_ID`) VALUES ('$PRODUCT_ID', '1', '$MEMBER_ID');";
+                     $insertrow = $tg->exec($sqlinsert);
+                     if($insertrow > 0){
+                        echo "新增成功!";
+                     }else{
+                        echo "新增失敗!";
+                     }  
+              }
+       //該名會員購物車沒有東西，直接加入       
+       }else{
+              $sqlinsert="INSERT INTO `CART` (`PRODUCT_ID`, `AMOUNT`, `MEMBER_ID`) VALUES ('$PRODUCT_ID', '1', '$MEMBER_ID');";
+                     $insertrow = $tg->exec($sqlinsert);
+                     if($insertrow > 0){
+                        echo "新增成功!";
+                     }else{
+                        echo "新增失敗!";
+                     }  
+       }
+       
+       
 
 ?>
--- joyo-vite/src/components/Index/HotProductIDX.vue
+++ joyo-vite/src/components/Index/HotProductIDX.vue
@@ -3,12 +3,12 @@
         <h1>熱門桌遊</h1>
         <h1 class="h1_sub">最熱門前 3 名桌遊</h1>
 
-        <ul class="index_hotProduct_list">
-            <li>
-                <RouterLink :to="product_data.product_info_url">
+        <ul class="index_hotProduct_list" v-if="productInfor.hotItem">
+            <li v-for="(list,index) in productInfor.hotItem" :key="index">
+                <RouterLink :to="productInfor.product_info_url">
                     <div class="index_hotProduct_list_topPInfo">
-                        <img :src="product_data.product_img_url" alt="">
-                        <div class="index_hotProduct_list_topPInfo_word">
+                        <img v-bind:src="list.IMG_URL" alt="">
+                        <div class="index_hotProduct_list_topPInfo_word" v-show="index==0">
                             <p>團隊合作最佳推薦！<br>
                                 蟬聯月熱銷排行 5 個月！<br>
                                 討論區熱門話題！</p>
@@ -18,14 +18,14 @@
                         </div>
                     </div>
                     <div class=".index_hotProduct_list_topPInfo product_list_pInfo">
-                        <h3>{{ product_data.name }}
-                            <p>{{ product_data.price }}</p>
+                        <h3>{{ list.NAME }}
+                            <p>${{ list.PRICE }}</p>
                         </h3>
-                        <AddCartBtn></AddCartBtn>
+                        <AddCartBtn :list="list"></AddCartBtn>
                     </div>
                 </RouterLink>
             </li>
-            <li>
+            <!-- <li>
                 <RouterLink :to="product_data.product_info_url">
                     <img :src="product_data.product_img_url" alt="">
                     <div class="product_list_pInfo">
@@ -46,22 +46,44 @@
                         <AddCartBtn></AddCartBtn>
                     </div>
                 </RouterLink>
-            </li>
+            </li> -->
         </ul>
     </div>
 </template>
 
 <script setup>
 // import AddCartBtn from '@/components/-UI_Element/AddCartBtn.vue'
-import { ref } from "vue"
-
-const product_data = ref({
-    product_info_url: '/productInfo/12345',
-    name: '璀璨寶石',
-    product_img_url: 'https://cdn.shopify.com/s/files/1/0513/4077/1515/products/scythe-board-game.jpg?v=1611090922',
-    price: '$116'
+import { ref ,onBeforeMount} from "vue";
+import axios from 'axios';
+//從資料庫取得銷售最多的商品(用but list篩選資料)
+// const product_data = ref({
+//     product_info_url: '/productInfo/12345',
+//     name: '璀璨寶石',
+//     product_img_url: 'https://cdn.shopify.com/s/files/1/0513/4077/1515/products/scythe-board-game.jpg?v=1611090922',
+//     price: '$116'
+// });
+const productInfor=ref({
+    hotItem:[],
+    product_info_url:'/productInfo/',
+});
+//利用axios取得資料庫桌遊資料
+const fetchData=()=>{
+    return axios.get('/api/index/getHotItem.php')
+        .then(res => {
+            //將資料庫回傳的資料存在tg變數中
+            productInfor.value.hotItem = res.data;
+            console.log(res.data);
+            console.log( productInfor.value.hotItem[0].PRICE);  
+        }
+        )
+        .catch(err => {
+            console.error(err);
+        });
+};
+onBeforeMount(() => {
+    
+    fetchData();
 })
-
 
 </script>
 
