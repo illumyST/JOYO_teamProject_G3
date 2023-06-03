52b0cd21ee6fcba0e21fe4bd611b3c4ff43f6155
jackyeh4488 <jackyeh4488@gmail.com>
2023-06-03 12:40:40 +0800

    00

--- joyo-vite/src/components/Index/BannerIDX.vue
+++ joyo-vite/src/components/Index/BannerIDX.vue
@@ -57,21 +57,12 @@
 //   })
 
 // onMounted(()=>{
-<<<<<<< HEAD
 //   let buttonArrow=document.querySelectorAll(".carousel__next")[0];
 //   // console.log(buttonArrow);
 //   buttonArrow.style.color='#513F2E';
 //   let buttonArro=document.querySelectorAll(".carousel__prev")[0];
 //   // console.log(buttonArrow);
 //   buttonArro.style.color='#513F2E';
-=======
-//   // let buttonArrow=document.querySelectorAll(".carousel__next")[0];
-//   // // console.log(buttonArrow);
-//   // buttonArrow.style.color='#513F2E';
-//   // let buttonArro=document.querySelectorAll(".carousel__prev")[0];
-//   // // console.log(buttonArrow);
-//   // buttonArro.style.color='#513F2E';
->>>>>>> jackyeh
 // }
 // )
   </script>
