2265f1beb41d484f2743494fcfff5af59d3bf71b
albert840823 <allen840419@gmail.com>
2023-06-09 14:35:43 +0800

    2023 06 09 yue 1

--- joyo-vite/src/components/-Global/header.vue
+++ joyo-vite/src/components/-Global/header.vue
@@ -8,7 +8,7 @@
                 <i class="fa-solid fa-magnifying-glass"></i>
                 <input class="header_nav_right_inputBox-input" type="text" v-model.trim="data.select" @keyup="searchItem">
                 <ul id="selectbox" v-if="searchBoxOpen">
-                    <li v-for="(item,index) in selectModel"><RouterLink :to="item.href" @click="searchBoxOpen = false;">{{ item.name }}</RouterLink></li>
+                    <li v-for="(item,index) in selectModel"><a :href="item.href" @click="searchBoxOpen = false;">{{ item.name }}</a></li>
                 </ul>
             </div>
             <ul class="header_nav_right" :class="{ '-on': nav_open === true }">
@@ -197,8 +197,8 @@ const searchItem = () => {
         if(responseData[n][1] != undefined){
             searchBoxOpen.value = true;
             let a =responseData[n][1]
-            selectModel.value.push({name:a,href:`productInfo/ID:${responseData[n][0]}`});
-            // console.log(selectModel.value);
+            selectModel.value.push({name:a,href:`http://localhost:5173/#/productInfo/ID:${responseData[n][0]}`});
+            console.log(selectModel.value);
         }else{
             searchBoxOpen.value = false;
         }
