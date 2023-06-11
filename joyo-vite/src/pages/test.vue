f516b17b9aa76018233e826a0146dc736a6936ee
yusinlan <you19921123@gmail.com>
2023-06-11 09:18:33 +0800

    新增寄送驗證碼功能

--- joyo-vite/src/pages/test.vue
+++ joyo-vite/src/pages/test.vue
@@ -1,19 +1,49 @@
 <template>
   <div>
-    我是測試
+    <button @click="sendMail()">我是測試</button>
   </div>
 </template>
 
 <script setup>
-import axios from 'axios';
-axios.get(`${import.meta.env.VITE_API_URL}/test/testMail.php`)
-  .then(res => {
-    alert(res.data);
-  });
-  axios.get(`${import.meta.env.VITE_API_URL}/test/test.php`)
-  .then(res => {
-    console.log(res.data);
-  });
+// import axios from 'axios';
+// axios.get(`${import.meta.env.VITE_API_URL}/test/testMail.php`)
+//   .then(res => {
+//     alert(res.data);
+//   });
+//   axios.get(`${import.meta.env.VITE_API_URL}/test/test.php`)
+//   .then(res => {
+//     console.log(res.data);
+//   });
+
+
+
+// Email.send({
+//     Host : "smtp.elasticemail.com",
+//     Username : "username",
+//     Password : "password",
+//     To : 'them@website.com',
+//     From : "you@isp.com",
+//     Subject : "This is the subject",
+//     Body : "And this is the body"
+// }).then(
+//   message => alert(message)
+// );
+
+const sendMail = () => {
+    Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "joyogamethd101@gamil.com",
+      Password : "catobidshquojtxy",
+      SecureToken: 'e40ecd2d-22e0-4511-ad53-c86d1ffcc494',
+      To: 'you19921123@gamil.com',
+      From: 'joyogamethd101@gamil.com',
+      Subject: '桌迷藏測試',
+      Body: '桌迷藏測試'
+    }).then(
+      message => alert(message)
+    )
+  };
+
 </script>
 
 <style lang="scss" scoped></style>
