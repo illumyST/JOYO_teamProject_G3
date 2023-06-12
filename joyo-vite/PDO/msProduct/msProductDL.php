cb8234de81596b48b2e424fb197fbebdd7aa54bb
yusinlan <you19921123@gmail.com>
2023-06-12 11:07:39 +0800

    修改SQL小寫變大寫

--- joyo-vite/PDO/msProduct/msProductDL.php
+++ joyo-vite/PDO/msProduct/msProductDL.php
@@ -5,7 +5,7 @@ $data = json_decode(file_get_contents('php://input'),true);
 $id = $data["pronum"];
 $name = $data["proname"];
 
-$sq1 = "DELETE FROM article_comment
+$sq1 = "DELETE FROM ARTICLE_COMMENT
 WHERE ARTICLE_ID in (SELECT ARTICLE_ID FROM ARTICLE WHERE GAME_NAME ='$name')" ;
 
 $sq11 = "DELETE FROM ARTICLE_like
@@ -13,7 +13,7 @@ WHERE ARTICLE_ID in (SELECT ARTICLE_ID FROM ARTICLE WHERE GAME_NAME ='$name')";
 
 $sq2 = "DELETE FROM ARTICLE WHERE GAME_NAME = '$name'" ;
 
-$sq3 = "DELETE from cart where product_id = '$id'";
+$sq3 = "DELETE from CART where PRODUCT_ID = '$id'";
 
 $sq4 = "DELETE FROM  PRODUCT_COMMENT WHERE PRODUCT_ID = '$id'" ;
 
